0e6df5588176f9e655906647a8ff19d6164258bc
Kevin Perez <unknotted26@gmail.com>
2016-04-02 03:55:37 -0400

    refactored main.cpp to NaiveBayesDriver.cpp

---
+++ NaiveBayesDriver.cpp
@@ -0,0 +1,27 @@
+/*
+ * NaiveBayesDriver.cpp
+ * Kevin Perez
+ *
+ *
+ *
+ */
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <ctime>
+
+// Forward declarations of function prototypes
+double BayesClassifier();
+
+int main()
+{
+  std::vector<int> value, column, row;
+
+  BayesClassifier();
+  return 0;
+}
+
+double BayesClassifier()
+{
+
+}
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <math.h>
-#include <vector>
-
-
-int main() {
-
-  return 0;
-}
